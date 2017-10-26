64cdb3288a9646c18489148769df0f23387390b1
DSPC-1\ChristianSwift <topman@dingnaiwen.xyz>
2017-10-26 19:52:32 +0800

    加入相册开关控制项

--- AlbumView/Home/Controller/IndexController.class.php
+++ AlbumView/Home/Controller/IndexController.class.php
@@ -17,9 +17,9 @@ class IndexController extends Controller {
 		$this -> assign('myalbum_thisyear',date('Y'));
 		//var_dump($info); //用于输出测试
 		//查询数据库中页面导航部分
-		$this -> navibar = M('myalbum_navi') -> select();
+		$this -> navibar = M('myalbum_navi') -> order("nid asc") -> select();
 		//查询数据库中相册封面部分
-		$this -> cover = M('myalbum_cover') -> select();
+		$this -> cover = M('myalbum_cover') -> where("open='%d'",1) -> order("cid asc") -> select();
 		$this -> display();
     }
 }
