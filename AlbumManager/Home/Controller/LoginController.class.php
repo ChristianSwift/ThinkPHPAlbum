6d68da54419bf0a189c85f7ca12b4f61859e0989
David Ding <ding@dingstudio.cn>
2017-11-01 19:08:36 +0800

    改动了登录注册模块的运行逻辑使之用于用户初始化

--- AlbumManager/Home/Controller/LoginController.class.php
+++ AlbumManager/Home/Controller/LoginController.class.php
@@ -3,6 +3,22 @@ namespace Home\Controller;
 use Think\Controller;
 class LoginController extends Controller {
   	public function index(){
+		//免登跳转逻辑
+		if(cookie("myalbum_token") != null || session("myalbum_token") != null){
+			if (cookie("myalbum_token") == session("myalbum_token")) {
+				$this->success('您已登录系统，正在为您自动跳转。',__ROOT__.'/admin.php', 2); //已登录时自动跳转至后台，避免多次反复登录
+				return true;
+			}
+		}
+		//检测用户数据库是否被初始化
+		$users = M('myalbum_users');
+		$userinfo = $users->select();
+		if($userinfo != null) {
+			$this -> assign('myalbum_regStatus','none');
+		}
+		else {
+			$this -> assign('myalbum_regStatus','block');
+		}
 		//查询网站基础内容
 		$basicinfo = M('myalbum_basicinfo');
 		$basicinfo = $basicinfo->select();
--- AlbumManager/Home/Controller/RegisterController.class.php
+++ AlbumManager/Home/Controller/RegisterController.class.php
@@ -3,6 +3,12 @@ namespace Home\Controller;
 use Think\Controller;
 class RegisterController extends Controller {
   	public function index(){
+		//检测用户数据库是否被初始化
+		$users = M('myalbum_users');
+		$userinfo = $users->select();
+		if($userinfo != null) {
+			$this->error('系统已经完成账户初始化，无需重复注册。',__ROOT__.'/admin.php?c=Login', 1); //尚未登录时跳回登录页面，防止URL输入恶意访问后台管理系统
+		}
 		//查询网站基础内容
 		$basicinfo = M('myalbum_basicinfo');
 		$basicinfo = $basicinfo->select();
