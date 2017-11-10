c00f4d3f21b317d303d60d3baf6a1ffe8ecc034f
ChristianSwift <topman@dingnaiwen.xyz>
2017-11-10 12:38:54 +0800

    导航功能

--- AlbumManager/Home/Controller/IndexController.class.php
+++ AlbumManager/Home/Controller/IndexController.class.php
@@ -16,15 +16,53 @@ class IndexController extends WebAuthorityController {
 		$this -> display();
 	}
 	public function navi(){
+		$basicinfo = M('myalbum_basicinfo');
+		$basicinfo = $basicinfo->select();
+		$basicinfo = $basicinfo[0];
+      	$this -> assign('myalbum_name',$basicinfo[myalbum_name]);
+		$this -> assign('myalbum_nickname',$basicinfo[myalbum_nickname]);
+		$this -> assign('myalbum_saying',$basicinfo[myalbum_saying]);
+      	$this -> assign('myalbum_author',$basicinfo[myalbum_author]);
+      	$this -> assign('myalbum_copyright',$basicinfo[myalbum_copyright]);
+		$this -> assign('myalbum_thisyear',date('Y'));
+		//查询数据库中页面导航部分
+		$this -> navibar = M('myalbum_navi') -> order("nid asc") -> select();
 		$this -> display('navi');
 	}
 	public function user(){
+		$basicinfo = M('myalbum_basicinfo');
+		$basicinfo = $basicinfo->select();
+		$basicinfo = $basicinfo[0];
+      	$this -> assign('myalbum_name',$basicinfo[myalbum_name]);
+		$this -> assign('myalbum_nickname',$basicinfo[myalbum_nickname]);
+		$this -> assign('myalbum_saying',$basicinfo[myalbum_saying]);
+      	$this -> assign('myalbum_author',$basicinfo[myalbum_author]);
+      	$this -> assign('myalbum_copyright',$basicinfo[myalbum_copyright]);
+		$this -> assign('myalbum_thisyear',date('Y'));
 		$this -> display('user');
 	}
 	public function album(){
+		$basicinfo = M('myalbum_basicinfo');
+		$basicinfo = $basicinfo->select();
+		$basicinfo = $basicinfo[0];
+      	$this -> assign('myalbum_name',$basicinfo[myalbum_name]);
+		$this -> assign('myalbum_nickname',$basicinfo[myalbum_nickname]);
+		$this -> assign('myalbum_saying',$basicinfo[myalbum_saying]);
+      	$this -> assign('myalbum_author',$basicinfo[myalbum_author]);
+      	$this -> assign('myalbum_copyright',$basicinfo[myalbum_copyright]);
+		$this -> assign('myalbum_thisyear',date('Y'));
 		$this -> display('album');
 	}
 	public function photo(){
+		$basicinfo = M('myalbum_basicinfo');
+		$basicinfo = $basicinfo->select();
+		$basicinfo = $basicinfo[0];
+      	$this -> assign('myalbum_name',$basicinfo[myalbum_name]);
+		$this -> assign('myalbum_nickname',$basicinfo[myalbum_nickname]);
+		$this -> assign('myalbum_saying',$basicinfo[myalbum_saying]);
+      	$this -> assign('myalbum_author',$basicinfo[myalbum_author]);
+      	$this -> assign('myalbum_copyright',$basicinfo[myalbum_copyright]);
+		$this -> assign('myalbum_thisyear',date('Y'));
 		$this -> display('photo');
 	}
 }
