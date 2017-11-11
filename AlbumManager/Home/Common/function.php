5dd0fee949cfc67ba373882a06367675b7f294c5
丁鼎 <ding@dingstudio.cn>
2017-11-11 18:14:57 +0800

    更新 'AlbumManager/Home/Common/function.php'

--- AlbumManager/Home/Common/function.php
+++ AlbumManager/Home/Common/function.php
@@ -1,6 +1,6 @@
 <?php
     function getGravatar($email,$s=128,$d='mm',$r='g',$img=false,$atts=array()){
-        $url = 'https://www.gravatar.com/avatar/';
+        $url = 'https://cn.gravatar.com/avatar/';
         $url .= md5( strtolower( trim( $email ) ) );
         $url .= "?s=$s&d=$d&r=$r";
         if ( $img ) {
